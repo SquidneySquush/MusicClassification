3b388b5bdc9bee6d3f541498038935c0f3c7aeb3
SquidneySquush <shnat@Sydneys-laptop.localdomain>
2021-04-07 17:06:05 -0400

    update

--- KMCSimp.py
+++ KMCSimp.py
@@ -12,13 +12,12 @@ fdmin = min(x for x in gen)
 print(gen, fdmin)
 print (np.shape(fdata)[1])
 
-def initial_centroids (data, k): # data is the array of arrays and k is the number of clusters
+def init_centroids (data, k): # data is the array of arrays and k is the number of clusters
     # get the number of columns (for fdata =2)
     n = np.shape(fdata)[1]
     # create empty centroid matrix 
     centroids = np.mat(np.zeros((k,n))) #np.zeros creates and ndarray -> np.mat turns it into a matix
-    print(type(centroids), centroids)
-
+    
     for i in range(n):
         min_i = min(fdata[:,i])
         max_i = max(fdata[:,i])
@@ -27,13 +26,67 @@ def initial_centroids (data, k): # data is the array of arrays and k is the numb
 
     return centroids
 
-def Euclidean_distance ():
+def Euclidean_distance (pt_one, pt_two):
     sq_dist = 0
-    for i in range(len(ft_one)):
-        sq_dist += (ft_one[i]-ft_two[i])**2
+    for i in range(len(pt_one)):
+        sq_dist += (pt_one[i]-pt_two[i])**2
     
     eDist= math.sqrt(sq_dist)
 
     return(eDist)
 
-initial_centroids(fdata, 3)
+#initial_centroids(fdata, 3)
+
+def cluster(data, k):
+    # number of rows in data array
+    m= np.shape(data)[0] 
+    print(m)
+    # instance cluster assignments
+    c_assignments = np.mat(np.zeros((m, 2))) 
+    print(c_assignments)
+    # initialize centroids
+    centroids = init_centroids(fdata, k)
+
+    #copy of original centroids
+    centroids_og = centroids.copy()
+
+    changed = True
+    counter = 0
+
+    # Loop until no cluster assignment changes
+    while changed:
+
+        changed = False
+        
+        # for every instance (row in dataset)
+        for i in range(m):
+
+            # Keep track of the minimum distance and minimum index for data
+            min_dist = np.inf
+            min_index = -1
+
+            for l in range(k):
+                # for every row (l) in the centroids matrix, calculate the 
+                # distance between row (i) in data 
+                dist_li = Euclidean_distance(centroids[l,:], data[i,:]) 
+                # if the calc distance is less then our tracked min_dist 
+                # set the min_dist to the calc dist and the min indext to 
+                # the current row (l)
+                if dist_li < min_dist:
+                    min_dist = dist_li
+                    min_index = l
+
+            # if our current row of c_assignments does not equal the min_index
+            # after the for loop then we change the c_assignments
+            if c_assignments[i,0] != min_index:
+                changed = True
+
+            # assign point to cluster and list distance
+            c_assignments[i, :] = min_index, min_dist
+
+        # find new centroid
+        for cent in range(k):
+            point = 
+
+
+cluster(fdata, 3)
